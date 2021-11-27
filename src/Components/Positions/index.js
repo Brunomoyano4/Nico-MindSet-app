dbf6ed1a2810e0d71f2dc78797f78a40faae5243
Bruno_Moyano <bruno_moyano@hotmail.com>
2021-11-27 03:28:37 -0300

    MS-39: fix eslinttrc and create read positions basic

--- src/Components/Positions/index.js
+++ src/Components/Positions/index.js
@@ -1,9 +1,24 @@
 import styles from './positions.module.css';
+import { useEffect, useState } from 'react';
 
 function Positions() {
+  const [positions, savePositions] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/positions`)
+      .then((response) => response.json())
+      .then((response) => {
+        savePositions(response);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Positions</h2>
+      <div>
+        {positions.map((positions) => {
+          return <div key={positions._id}> {positions.job} </div>;
+        })}
+      </div>
     </section>
   );
 }
