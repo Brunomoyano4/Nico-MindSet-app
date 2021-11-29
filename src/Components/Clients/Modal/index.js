db3ea97d1e27dfb0b015a2c79c93eecbe5d5d0e3
ripellizzari <ripellizzari@gmail.com>
2021-11-29 02:58:49 -0300

    MS-33: fix modal & delete function

--- src/Components/Clients/Modal/index.js
+++ src/Components/Clients/Modal/index.js
@@ -1,14 +1,27 @@
-// import styles from './modal.module.css';
+import styles from './modal.module.css';
 
-// function Modal({ props }) {
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.Modal}>
-//         <h3>Modal</h3>
-//         <button onClick={props.closeModal}></button>
-//       </div>
-//     </div>
-//   );
-// }
+function Modal(props) {
+  if (!props.show) {
+    return null;
+  }
+  const onCloseModal = () => {
+    props.onCloseModal();
+    props.closeModal();
+  };
 
-// export default Modal;
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <h2>YOU ARE ABOUT TO DELETE A CLIENT</h2>
+        <button className={styles.btnModal} onClick={onCloseModal}>
+          Cancel
+        </button>
+        <button className={styles.btnModal} onClick={props.onConfirm}>
+          Confirm
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
