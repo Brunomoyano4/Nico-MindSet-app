f61d505c83b105e402104ae337c3a5906958ede9
Martin Raveglia <ravegliamartin@gmail.com>
2021-12-06 10:18:55 -0300

    MS-51: Add loading logic to Profiles List

--- src/Components/Profiles/index.js
+++ src/Components/Profiles/index.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import ListItem from './ListItem';
 import DeleteBtn from './DeleteBtn';
 import Modal from './Modal';
+import LoadingSpinner from '../Shared/LoadingSpinner';
 
 function Profiles() {
   const tableHeaderItems = ['Branch', 'Name', 'Description', ''];
@@ -10,6 +11,7 @@ function Profiles() {
   const [showModal, setShowModal] = useState(false);
   const [modalSubtitle, setModalSubtitle] = useState(['modalSubtitle']);
   const [deleteId, setDeleteId] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const deleteProfile = (e, id, branch, name) => {
     e.stopPropagation();
@@ -22,13 +24,15 @@ function Profiles() {
     window.location.href = id ? `/profiles/form?id=${id}` : '/profiles/form';
   };
   const getProfiles = () => {
+    setLoading(true);
     fetch(`${process.env.REACT_APP_API}/profiles`)
       .then((res) => {
         return res.json();
       })
       .then((res) => {
         setProfiles(res);
-      });
+      })
+      .finally(() => setLoading(false));
   };
   useEffect(getProfiles, []);
 
@@ -47,23 +51,27 @@ function Profiles() {
       <h2>Profiles</h2>
       <table className={styles.list}>
         <ListItem headerItems={tableHeaderItems} />
-        <tbody className={styles.tableBody}>
-          {profiles.map(({ _id, profileName, branch, description }) => {
-            const deleteBtn = (
-              <DeleteBtn onClick={(e) => deleteProfile(e, _id, branch, profileName)} />
-            );
-            const tableListItems = [branch, profileName, description, deleteBtn];
-            return (
-              <ListItem
-                key={_id}
-                listItems={tableListItems}
-                id={_id}
-                onRowClick={() => toForm(_id)}
-              />
-            );
-          })}
-        </tbody>
+        {!loading && (
+          <tbody className={styles.tableBody}>
+            {profiles.map(({ _id, profileName, branch, description }) => {
+              const deleteBtn = (
+                <DeleteBtn onClick={(e) => deleteProfile(e, _id, branch, profileName)} />
+              );
+              const tableListItems = [branch, profileName, description, deleteBtn];
+              return (
+                <ListItem
+                  key={_id}
+                  listItems={tableListItems}
+                  id={_id}
+                  onRowClick={() => toForm(_id)}
+                />
+              );
+            })}
+          </tbody>
+        )}
       </table>
+      {loading && <LoadingSpinner circle={false} />}
+      {!loading && !profiles.length && <h3 className={styles.nothingHere}>Oops... Nothing Here</h3>}
       <button className={styles.addBtn} type="button" onClick={() => toForm()}>
         Add Profile
       </button>
