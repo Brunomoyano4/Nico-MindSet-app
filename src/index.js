3eb2e9664ab38cff018a9d8a2b13ba55e8a698b3
Bruno_Moyano <bruno_moyano@hotmail.com>
2021-12-11 01:56:07 -0300

    add redux devtools and create folders structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import Routes from './routes';
+import rootReducer from './redux/reducers/rootReducer';
 import './reset.css';
 import './index.css';
 
+const configureStore = () => {
+  const enhancer = composeWithDevTools(applyMiddleware(thunk));
+  return createStore(rootReducer, enhancer);
+};
+
+const store = configureStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
