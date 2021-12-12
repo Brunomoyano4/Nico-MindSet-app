c585c8192cf81c0c0df9748959e6870183b97db3
Martin Raveglia <ravegliamartin@gmail.com>
2021-12-12 19:59:17 -0300

    MS-63: Add global state and refactor client component

--- src/Components/Clients/Client/index.js
+++ src/Components/Clients/Client/index.js
@@ -1,10 +1,13 @@
-import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import styles from '../Client/client.module.css';
+import { useState } from 'react';
 import DeleteBtn from '../../Shared/DeleteBtn/index';
 import Modal from '../../Shared/Modal';
+import styles from '../Client/client.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteClient } from '../../../redux/clients/thunks';
 
 const Client = ({ client }) => {
+  const dispatch = useDispatch();
   const [showConfirmModal, setShowConfirmModal] = useState(false);
   const [error, setError] = useState('');
   const history = useHistory();
@@ -13,28 +16,14 @@ const Client = ({ client }) => {
     history.push(`/clients/form?id=${client._id}`);
   };
 
-  const onDelete = (event) => {
-    const url = `${process.env.REACT_APP_API}/clients/${client._id}`;
-    event.stopPropagation();
-    fetch(url, {
-      method: 'DELETE'
-    })
-      .then((res) => {
-        if (res.status !== 200) {
-          return res.json().then((message) => {
-            throw new Error(message);
-          });
-        }
-        return (window.location.href = '/clients');
-      })
-      .catch((error) => setError(error.toString()));
-  };
-
   return (
     <>
       <Modal
-        title="You're about to delete a client"
-        onConfirm={onDelete}
+        title="Are you sure you want to delete a Client?"
+        onConfirm={(e) => {
+          e.stopPropagation();
+          dispatch(deleteClient(client._id));
+        }}
         show={showConfirmModal}
         closeModal={() => setShowConfirmModal(false)}
         type={'Confirm'}
