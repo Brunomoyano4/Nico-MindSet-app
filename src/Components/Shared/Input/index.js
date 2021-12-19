37a3dc7258b22f5a2bcd47d6db1909a68bf5f351
Bruno_Moyano <bruno_moyano@hotmail.com>
2021-12-19 19:55:41 -0300

    MS-82: implement final-form in admins

--- src/Components/Shared/Input/index.js
+++ src/Components/Shared/Input/index.js
@@ -5,15 +5,16 @@ const Input = (props) => (
     {props.label && <label className={styles.label}>{props.label}</label>}
     <input
       className={styles.input}
-      type={props.type ?? 'text'}
-      id={props.id}
-      name={props.name}
-      value={props.value}
-      required={props.required}
-      onChange={props.onChange}
+      // type={props.type ?? 'text'}
+      // id={props.id}
+      // name={props.name}
+      // value={props.value}
+      // required={props.required}
+      // onChange={props.onChange}
       placeholder={props.placeholder}
-      pattern={props.pattern}
+      // pattern={props.pattern}
       disabled={props.disabled}
+      {...props.input}
     />
   </>
 );
